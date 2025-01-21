e9cda8b24b293506344adace7472366c6803523b
Dario <dario.c2000@libero.it>
2025-01-21 15:25:17 +0100

    Fixed registration/login logic

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -11,6 +11,6 @@ export class UserService {
   constructor(private http: HttpClient) { }
 
   saveUser(user: any): Observable<any> {
-    return this.http.post<any>(`${this.apiBaseUrl}/signup`, user);
+    return this.http.post<any>(`${this.apiBaseUrl}/signup`, JSON.stringify(user));
   }
 }
